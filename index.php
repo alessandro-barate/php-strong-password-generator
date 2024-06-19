35936fe3c7113d2a270d82e50f879dd4a6064f45
alessandro-barate <alebarat@gmail.com>
2024-06-19 15:20:52 +0100

    [UPD] Including functions file

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
-
-// $generated_password = $_GET['generated-password'];
-if (isset($_GET['length'])) {
-    $length = $_GET['length'];
-    $generated_password = generatePassword($length);
-};
-
-function generatePassword($length)
-{
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-_+<>[]{}';
-    $my_password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $my_password .= $characters[rand(0, strlen($characters) - 1)];
-    }
-    return $my_password;
-};
-
+include 'functions.php';
 ?>
 
 <!DOCTYPE html>
