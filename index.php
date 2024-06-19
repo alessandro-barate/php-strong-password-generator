e0e286aaee403655f3ccb440cf1023113b9dedb8
alessandro-barate <alebarat@gmail.com>
2024-06-19 15:03:41 +0100

    [UPD] Issues with the logic

--- index.php
+++ index.php
@@ -6,13 +6,16 @@ if (isset($_GET['length'])) {
     $generated_password = generatePassword($length);
 };
 
-function generatePassword($length){
+function generatePassword($length)
+{
     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-_+<>[]{}';
     $my_password = '';
     for ($i = 0; $i < $length; $i++) {
-        $my_password = $characters
+        $my_password = $characters[rand(0, strlen($characters) - 1)];
     }
-}
+    echo ($my_password);
+    return $my_password;
+};
 
 ?>
 
